b2ae35f8909e33d6b11ec1f59cd106d9c26235cc
Vitalii <vitaliimusiienko10@gmail.com>
2025-12-27 13:05:37 +0200

    connect linked-list and knapsack commands in main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fractal.commands import register_commands as register_fractal
 from graphs.commands import register_commands as register_graphs
 from heap_visualization.commands import register_commands as register_heap_visualization
 from tree_traversal.commands import register_commands as register_traversal
+from knapsack.commands import register_commands as register_knapsack
 
 def main():
     parser = argparse.ArgumentParser(
@@ -17,6 +18,7 @@ def main():
     register_graphs(subparsers)
     register_heap_visualization(subparsers)
     register_traversal(subparsers)
+    register_knapsack(subparsers)
     
     args = parser.parse_args()
 
